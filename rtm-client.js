cb277cf575eeae680e02170d0b1214b0069c0631
Mika Reyes <reyes.mikaelahelene@gmail.com>
2017-08-01 10:59:02 -0700

    fixed the axios post route

--- rtm-client.js
+++ rtm-client.js
@@ -4,12 +4,17 @@ var RTM_EVENTS = require('@slack/client').RTM_EVENTS;
 var CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
 var botToken = process.env.BOT_USER_TOKEN || '';
 var apiToken = process.env.SLACK_API_TOKEN || '';
+var aiToken = process.env.API_AI_TOKEN || '';
 var rtm = new RtmClient(botToken);
-var apiai = require('apiai');
-var app = apiai(process.env.API_AI_TOKEN);
 var web = new WebClient(apiToken);
+
 var express = require('express');
 var router = express();
+var path = require('path');
+
+router.use(express.static(path.join(__dirname, 'public')))
+
+var axios = require('axios');
 let channel;
 let responseMsg;
 
@@ -25,69 +30,66 @@ rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, function() {
 })
 
 rtm.on(RTM_EVENTS.MESSAGE, function handleRtmMessage(message) {
-  var request = app.textRequest(message.text, {
-    sessionId: '6fd6f06f-c81d-4484-92b3-fe3e2afb3222'
-  });
-
-  // request.on('response', function(response) {
-  //     responseMsg = response.result.fulfillment.speech;
-  //     rtm.sendMessage(responseMsg, message.channel);
-  // })
-
-  request.on('response', function(response) {
-      web.chat.postMessage(message.channel, 'Title of the thing', {
-        "text": "Would you like to play a game?",
-        "attachments": [
-            {
-                "text": "Choose a game to play",
-                "fallback": "You are unable to choose a game",
-                "callback_id": "wopr_game",
-                "color": "#3AA3E3",
-                "attachment_type": "default",
-                "actions": [
-                    {
-                        "name": "game",
-                        "text": "Falken's Maze",
-                        "type": "button",
-                        "value": "maze"
-                    },
-                    {
-                        "name": "game",
-                        "text": "Thermonuclear War",
-                        "style": "danger",
-                        "type": "button",
-                        "value": "war",
-                        "confirm": {
-                            "title": "Are you sure?",
-                            "text": "Wouldn't you prefer a good game of chess?",
-                            "ok_text": "Yes",
-                            "dismiss_text": "No"
-                        }
-                    }
-                ]
-            }
-        ]
-        }, function(err, res) {
-            if (err) {
-                console.log('err:', err);
-            } else {
-              console.log('Message sent: ', res);
-            }
-      });
+  axios({
+      method: 'post',
+      url: 'https://api.api.ai/v1/query?v=20150910',
+      headers: {
+          'Authorization': aiToken,
+          'Content-Type': 'application/json; charset=utf-8'
+      },
+      data: {
+          query: [message.text],
+          lang: "en",
+          sessionId: '6fd6f06f-c81d-4484-92b3-fe3e2afb3222',
+      },
   })
-  console.log('out');
-  request.on('error', function(error) {
-      console.log(error);
-  });
-  request.end();
-
-
+  .then((response) => {
+    //   console.log(response);
+      if (message.subtype !== "bot_message") {
+          web.chat.postMessage(message.channel, 'Scheduler Bot', {
+            "text": "Scheduler Bot",
+            "username": "PamSpam2",
+            "attachments": [
+                {
+                    "text": "Would you like to schedule a meeting?",
+                    "fallback": "You are unable to choose a game",
+                    "callback_id": "wopr_game",
+                    "color": "#3AA3E3",
+                    "attachment_type": "default",
+                    "actions": [
+                        {
+                            "name": "schedule",
+                            "text": "Yes",
+                            "type": "button",
+                            "value": "schedule"
+                        },
+                        {
+                            "name": "schedule",
+                            "text": "Cancel",
+                            "style": "danger",
+                            "type": "button",
+                            "value": "cancel",
+                            // "confirm": {}
+                        }
+                    ]
+                }
+            ]
+            })
+            .then((webMessage) => {
+                console.log(webMessage);
+            })
+      }
+  })
 });
 
 
 rtm.start();
 
-router.post('/interactive', function(req, res) {
+router.get('/interactive', function(req, res) {
     res.send("hello the API works");
-    console.log('im.replies', req)
+    console.log('im.replies', req.query)
+});
+
+router.listen(8080, function() {
+    console.log('PamSpam2 listening on port 8080.');
 });
