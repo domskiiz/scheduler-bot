b91d7ccbcd2c394cd6a868d8bfc213adae2acfad
Alissa Niewiadomski <alissaaadomski@gmail.com>
2017-08-02 15:43:26 -0700

    small changes to saveEvents

--- saveEvents/saveMeeting.js
+++ saveEvents/saveMeeting.js
@@ -9,33 +9,18 @@ var createMeeting = require('../createEvents/meeting');
 var oauthRoute = require('../oauthRoute');
 var oauth2Client = oauthRoute.oauth2Client;
 
-saveMeeting = function(user) {
-  oauth2Client.setCredentials(
-    {
-      access_token: user[0].GoogleAccessToken,
-      refresh_token:  user[0].GoogleRefreshToken,
-    }
-  );
-  oauth2Client.refreshAccessToken(function(err, tokens) {
-    oauth2Client.setCredentials(
-      {
-        access_token: tokens.access_token,
-        refresh_token: tokens.refresh_token,
+saveMeeting = function(meeting, date, time, attendees) {
+  var event = createMeeting(meeting, date, time, attendees)
+  calendar.events.insert({
+      auth: oauth2Client,
+      calendarId: 'primary',
+      resource: event,
+    }, function(err, event) {
+      if (err) {
+        console.log('There was an error contacting the Calendar service: ' + err);
+        return;
       }
-    );
-    var event = createMeeting('hey', '2017-08-02', '17:00:00', [{ 'email' : 'alissaaadomski@gmail.com'}])
-    calendar.events.insert({
-        auth: oauth2Client,
-        calendarId: 'primary',
-        resource: event,
-      }, function(err, event) {
-        if (err) {
-          console.log('There was an error contacting the Calendar service: ' + err);
-          return;
-        }
-      console.log('Event created: %s', event.htmlLink);
-      }
-    );
+    console.log('Event created: %s', event.htmlLink);
   });
 }
 
