85d551749e1d52ddb1376c8875d910b52ea59357
Alissa Niewiadomski <alissaaadomski@gmail.com>
2017-08-03 12:06:39 -0700

    oauth saves emails in db

--- oauthRoute.js
+++ oauthRoute.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var google = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
 var calendar = google.calendar('v3');
+var plus = google.plus('v1');
 
 var router = express.Router();
 
@@ -15,14 +16,14 @@ var Task = require('./models').Task;
 
 var SlackId;
 
-// connect to our credentials
+// connect to app's credentials
 var oauth2Client = new OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
   process.env.DOMAIN
 );
 
-//renders the consent page
+// renders the consent page
 router.get('/connect/', function(req, res) {
   SlackId = req.query.SlackId;
   var url = oauth2Client.generateAuthUrl({
@@ -30,7 +31,8 @@ router.get('/connect/', function(req, res) {
     prompt: 'consent',
     scope: [
       'https://www.googleapis.com/auth/userinfo.profile',
-      'https://www.googleapis.com/auth/calendar'
+      'https://www.googleapis.com/auth/calendar',
+      'email'
     ],
     state: encodeURIComponent(JSON.stringify({
       auth_id: req.query.auth_id
@@ -39,26 +41,34 @@ router.get('/connect/', function(req, res) {
   res.redirect(url);
 });
 
-// upon success, save tokens in database
+// get the authentication page and save credentials in database
 router.get('/auth', function(req, res) {
-  console.log(req.query.code);
   res.send('success');
+  // get tokens from auth code given back in url
   oauth2Client.getToken(req.query.code, function(err, tokens) {
     if (!err) {
+      var email = '';
       oauth2Client.setCredentials(tokens);
-      User.findOneAndUpdate({
-        SlackId: SlackId
-      }, {$set:{
-        GoogleAccessToken: tokens.access_token,
-        GoogleRefreshToken: tokens.refresh_token,
-        GooglePprofileId: tokens.id_token,
-        GoogleCode: req.query.code
-      }}, function(err,doc) {
-        console.log(doc);
+      plus.people.get({
+        auth: oauth2Client,
+        userId: 'me'
+      }, function(err, user) {
+        email = user.emails[0].value;
+        User.findOneAndUpdate({
+          SlackId: SlackId
+        }, {$set:{
+          GoogleAccessToken: tokens.access_token,
+          GoogleRefreshToken: tokens.refresh_token,
+          GooglePprofileId: tokens.id_token,
+          GoogleCode: req.query.code,
+          SlackEmail: email
+        }}, function(err,doc) {
+          console.log(doc);
+        });
       });
     }
-  })
-})
+  });
+});
 
 module.exports = {
   router,
